7c6af716fdb344374a212acd4c44ed4ffe40ad71
Piyush Ramteke <piyu.143247@gmail.com>
2026-09-02 06:05:20 +0530

    feat: render universe environment on home page

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { UniverseScene } from "@/components/3d/UniverseScene";
+
+export default function Home() {
+  return (
+    <main className="universe-shell">
+      <UniverseScene />
+    </main>
+  );
+}
